ff59ee0dd593dd96a3c5764ad60d1d82f20be2c6
Will <asampongg377@gmail.com>
2024-05-11 07:54:37 +0000

    Init

---
+++ Luxury-Hotels-Website/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Navbar';
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ Luxury-Hotels-Website/src/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-evenly w-3/4 mx-auto h-32">
+        <div className="logo w-64 h-36 bg-[#E0B973] rounded-bl-3xl rounded-br-3xl">
+            <div className="logo-text pt-8">
+                <div className="text-[#14274A] font-bold uppercase text-4xl text-center">
+                    luxury
+                </div>
+                <div className="uppercase text-[#14274A] font-bold text-sm text-center">
+                    hotels
+                </div>
+            </div>
+        </div>
+        <ul className="nav-list flex justify-between w-1/3 my-auto">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/facilities">Facilities</Link></li>
+            <li><Link to="/rooms">Rooms</Link></li>
+            <li><Link to="/contact-us">Contact Us</Link></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
