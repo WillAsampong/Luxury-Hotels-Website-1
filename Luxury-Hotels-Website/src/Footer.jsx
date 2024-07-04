e6da59fa00f1942121f1456f18d0c93b92133a72
Will <asampongg377@gmail.com>
2024-07-04 15:24:30 +0000

    Init

--- Luxury-Hotels-Website/src/Footer.jsx
+++ Luxury-Hotels-Website/src/Footer.jsx
@@ -3,31 +3,31 @@ import { faFacebookF, faTwitter, faSquareInstagram } from "@fortawesome/free-bra
 
 const Footer = () => {
   return (
-    <div className="bg-[#14274A] w-full h-48">
+    <div className="bg-[#14274A] w-full h-48 relative">
         <footer className="flex text-white justify-between w-4/5 mx-auto items-center">
             <div className="footer-group1">
                 <div className="logo-text">
-                    <div className="uppercase text-white text-4xl font-bold">
+                    <div className="uppercase text-white text-3xl font-bold">
                         Luxury
                     </div>
                     <div className="uppercase text-white font-normal text-sm tracking-[0.3rem]">
                         Hotels
                     </div>
                 </div>
-                <div className="reach-out mt-4 te">
+                <div className="reach-out mt-4 text-sm">
                     <p>497 Evergreen Rd. Roseville, CA 95673</p>
                     <p>+44 345 678 903</p>
                     <p>luxury_hotels@gamil.com</p>
                 </div>
             </div>
             <div className="nav">
-                <ul className="space-y-4 font-normal">
+                <ul className="space-y-2 font-normal text-sm">
                     <li className="cursor-pointer">About</li>
                     <li className="cursor-pointer">Contact</li>
                     <li className="cursor-pointer">Terms & Conditions</li> 
                 </ul>
             </div>
-            <div className="socials space-y-2">
+            <div className="socials space-y-2 text-sm">
                 <div className="facebook flex space-x-4 cursor-pointer">
                     <FontAwesomeIcon icon={faFacebookF} className="mt-1"/>
                     <div className="social-text">
@@ -47,7 +47,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="subscribe">
+            <div className="subscribe text-sm">
                 <p className="mb-4">Subscribe to our newsletter</p>
                 <div className="email-group border-[#E0B973] border-2 rounded-sm">
                     <input type="text" placeholder="Email Address" className="border-none outline-none py-3 px-4 bg-transparent"/>
--- Luxury-Hotels-Website/src/Pages/Home.jsx
+++ Luxury-Hotels-Website/src/Pages/Home.jsx
@@ -1,43 +1,41 @@
+import '../App.css'
 import HomeHero from '../assets/home-hero.png'
-import Navbar from '../Navbar'
+import HotelRoom from '../assets/hotel-room.jpg'
+import Beach from '../assets/beach.jpg'
 import Footer from '../Footer'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faChevronDown } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import ExploreCardComponent from './ExploreCardComponent'
+import HeroSection from './HeroSection'
+
 
 const Home = () => {
   return (
     <div>
-        <div
-         style={{backgroundImage: `url(${HomeHero})`}} 
-         className="bg-slate-600 
-         bg-blend-overlay 
-         w-full 
-         h-screen 
-         bg-cover 
-         bg-center 
-         flex 
-         flex-col">
-            <Navbar />
-                <div className="hero w-4/5 mx-auto mt-20">
-                    <div className="hero-content w-2/5">
-                        <p className='text-white text-4xl uppercase footer font-light'>Welcome to</p>
-                        <p className='uppercase text-8xl text-white logo-text tracking-wider'>Luxury</p>
-                        <p className='uppercase text-white tracking-[0.6rem] logo-text text-4xl font-bold'>Hotels</p>
-                        <p className='text-white footer text-xl'>Book your stay and enjoy Luxury <br/> redefined at the most affordable rates.</p>
-                    </div>
-                    <div className="cta">
-                        <button className='py-3 px-6 mt-10 bg-[#E0B973] footer rounded-md'>
-                            <div className="book-now-icon"></div>
-                            <div className="book-now uppercase text-white">Book now</div>
-                        </button>
-                    </div>
-                    <div className="scroll">
-                        <p className='footer text-white font-semibold'>Scroll</p>
-                        <div className="scroll-icon">
-                            {/* <FontAwesomeIcon icon={faChevronDown} /> */}
-                        </div>
-                    </div>
+        <HeroSection img={HomeHero}/>
+        <div>
+            <p className='footer text-2xl text-[#14274A] flex justify-center my-8'>All our room types are including complementary breakfast</p>
+            <ExploreCardComponent
+             title="Luxury redefined" 
+             text="Our rooms are designed to transport you into an environment made for leisure. Take your mind off the day-to-day of home life and find a private paradise for yourself."
+             img={HotelRoom}/>
+            <ExploreCardComponent
+             title="Leave your worries in the sand" 
+             text="We love life at the beach. Being close to the ocean with access to endless sandy beach ensures a relaxed state of mind. It seems like time stands still watching the ocean."
+             img={Beach}/>
+        </div>
+        <div className="testimonials flex flex-col items-center gap-y-4 mb-10">
+            <h2 className='font-bold text-[#14274A] text-4xl'>Testimonials</h2>
+            <p className='footer text-[#14274A] text-xl font-medium'>{`"Calm, Serene, Retro - What a way to relax and enjoy"`}</p>
+            <p className=''>Mr. and Mrs. Baxter, UK</p>
+            <div className="next-prev-btns flex gap-x-2">
+                <div className="prev-btn">
+                    <FontAwesomeIcon icon={faChevronLeft} className='bg-[#E0B973] px-3 py-2 text-white rounded-lg'/>
                 </div>
+                <div className="next-btn">
+                    <FontAwesomeIcon icon={faChevronRight} className='bg-[#E0B973] px-3 py-2 text-white rounded-lg'/>
+                </div>
+            </div>
         </div>
         <Footer />
     </div>
